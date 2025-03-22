8deae2fd6ed03c38b913e05d3396170a3b0cb8ec
cbgpenguin <cbaneygiampoala@gmail.com>
2025-03-21 22:22:56 -0400

    background placeholder and stuff

--- animatedSprite.py
+++ animatedSprite.py
@@ -1,9 +1,8 @@
-    
-
 import pygame
 
 
 class AnimatedSprite(pygame.sprite.Sprite):
+        
         def __init__(self, screen, x, y, defaultFrames, frameRate):
             super().__init__()
             self.isFlipped = False
@@ -15,44 +14,44 @@ class AnimatedSprite(pygame.sprite.Sprite):
             self.frame_index = 0
             self.frame_rate = frameRate
             self.frame_counter = 0
-            self.movePos = [0,0]
+            self.movePos = [0, 0]
             self.area = screen.get_rect()
             self.speed = 6
             self.state = "still"
             self.clock = pygame.time.Clock()
             self.lastUpdate = 0
-
-
         
         def update(self):
             print(self.isFlipped)
             delta = self.clock.tick() / 1000.0
             self.lastUpdate = self.lastUpdate + delta
             # print(self, "frame update:", self.lastUpdate)
-            if self.lastUpdate >= (1.0/self.frame_rate):
+            if self.lastUpdate >= (1.0 / self.frame_rate):
                 # For example have a frame rate of 6 fps: (1/6 = 0.16666) so if its been longer go to next frame
                 self.lastUpdate = 0
                 self.frame_index = (self.frame_index + 1) % len(self.frames)
                 self.image = self.frames[self.frame_index]
+
             newPos = self.rect.move(self.movePos)
             if self.area.contains(newPos):
                 self.rect = newPos
                 pygame.event.pump()
             print("state:", self.state)
+
             if self.state == "moveLeft" and self.isFlipped == False:
                 self.image = pygame.transform.flip(self.image, True, False)
                 print("flipping")
                 self.isFlipped = True
             elif self.state != "moveLeft" and self.isFlipped == True:
-                 self.image = pygame.transform.flip(self.image, True, False)
-                 print("unfliping!")
-                 self.isFlipped = False
+                self.image = pygame.transform.flip(self.image, True, False)
+                print("unFlipping!")
+                self.isFlipped = False
 
         def changeAnimation(self, newFrames):
-             self.frames = newFrames
+            self.frames = newFrames
         
         def defaultAnimation(self):
-             self.frames = self.defaultFrames
+            self.frames = self.defaultFrames
 
         def moveLeft(self):
             self.movePos[0] = self.movePos[0] - (self.speed)
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import pygame
 import animatedSprite
 
 
-
 def main():
-    clock = pygame.time.Clock()
+    # clock = pygame.time.Clock()
     scale = 2
     # Initialise screen
     pygame.init()
@@ -12,17 +11,16 @@ def main():
     pygame.display.set_caption('Detective Game')
 
     # Fill background
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((30, 30, 250))
+    # background = pygame.Surface(screen.get_size())
+    # background = background.convert()
+    background = loadAndScaleImage("resources/backdrop1.png")
 
-    # Display some text
+    # Display text
     font = pygame.font.Font(None, 36)
     text = font.render("Objective: Find evidence", 1, (10, 255, 10))
     textPos = text.get_rect()
     textPos.centerx = background.get_rect().centerx
     background.blit(text, textPos)
-
 
     # make Player: object of playerSprite
     playerSpriteSheet = loadAndScaleImage("resources/playerFiles/playerStandSpriteSheet.png")
@@ -31,13 +29,10 @@ def main():
     playerWalkSpriteSheet = loadAndScaleImage("resources/playerFiles/detective-walk-animation-place-holder.png")
     playerWalkSpriteFrames = load_sprite_sheet(playerWalkSpriteSheet, 121 * scale, 200 * scale, 1, 1, False)
 
-
-    
-
     # Event loop
     while True:
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        delta = clock.tick() / 1000.0
+        # delta = clock.tick() / 1000.0
         # print("event update:", delta)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -53,7 +48,7 @@ def main():
                     playerSprite.changeAnimation(playerWalkSpriteFrames)
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    playerSprite.movePos = [0,0]
+                    playerSprite.movePos = [0, 0]
                     playerSprite.state = "still"
                     playerSprite.defaultAnimation()
             
@@ -82,11 +77,11 @@ def load_sprite_sheet(spriteSheet, frameWidth, frameHeight, rows, columns, bounc
 
     if bounce:
         for bounceRow in range(rows):
-            realRow = (rows-1 - bounceRow)
+            realRow = (rows - 1 - bounceRow)
             # print("bounce row:", bounceRow)
             # print("real row:", realRow)
             for bounceColumn in range(columns):
-                realColumn = ((columns-1) - bounceColumn)
+                realColumn = ((columns - 1) - bounceColumn)
                 # print("bounce column is:", bounceColumn)
                 # print("real column:", realColumn)
                 if bounceColumn != 0:
@@ -100,19 +95,21 @@ def load_sprite_sheet(spriteSheet, frameWidth, frameHeight, rows, columns, bounc
             
     return frames
 
+
 def loadAndScaleImage(originalImagePath):
-    scaled_image = -1
+    scaledImage = -1
     try:
         originalImage = pygame.image.load(originalImagePath)
     except:
         # print("failed to load resource about to crash")
-        return -1 # ERROR
+        return -1  # ERROR
     
     newWidth = originalImage.get_width() * 2
     newHeight = originalImage.get_height() * 2
-    scaled_image = pygame.transform.scale(originalImage, (newWidth, newHeight))
+    scaledImage = pygame.transform.scale(originalImage, (newWidth, newHeight))
 
-    return scaled_image
+    return scaledImage
+
+
 main()
-
 
